a748d58551caf76975ca6e04ed1636d5d7ed3812
HongTr <truongminhhong0306@gmail.com>
2023-03-30 20:35:06 +0700

    add images

--- name.py
+++ name.py
@@ -1,18 +1,31 @@
 import csv
+import json
 
-# Open the CSV file for reading
-with open('data.csv', 'r') as csvfile:
-    reader = csv.DictReader(csvfile)
+csvfile = open('output.csv', 'r')
+jsonfile = open('output.json', 'w')
 
-    # Open a new CSV file for writing with an additional column for the image filename
-    with open('data.csv', 'w', newline='') as outfile:
-        fieldnames = reader.fieldnames + ['image_filename']
-        writer = csv.DictWriter(outfile, fieldnames=fieldnames)
-        writer.writeheader()
+fieldnames = ('id', 'name', 'number', 'position', 'image')
+reader = csv.DictReader(csvfile, fieldnames, delimiter=",")
+next(reader)  # skip header row
 
-        # Iterate over each row in the input CSV file, add the image filename, and write to the output CSV file
-        for row in reader:
-            lastname = row['name'].split()[-1].lower() # Get the last name and convert to lowercase
-            image_filename = f"{lastname}.jpg"
-            row['image_filename'] = image_filename
-            writer.writerow(row)
+json.dump([row for row in reader], jsonfile, indent=4)
+
+csvfile.close()
+jsonfile.close()
+# import csv
+# import os
+
+# with open('data.csv', 'r') as csvfile:
+#     reader = csv.DictReader(csvfile, delimiter="\t")
+
+#     for row in reader:
+#         name = row['name']
+#         last_word = name.split()[-1].lower()
+#         old_filename = row['image']
+#         new_filename = f"images/{last_word}.jpg"
+#         row['image'] = new_filename
+
+#         with open('output.csv', 'a', newline='') as outfile:
+#             writer = csv.DictWriter(outfile, fieldnames=row.keys())
+#             writer.writerow(row)
+
